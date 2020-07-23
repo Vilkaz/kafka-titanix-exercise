1c29975228b57994471c0459df81324c669ab372
UA2LFHQ <vilius.kukanauskas@vwgis.de>
2020-07-23 12:38:45 +0200

    made generated class fields private

--- src/main/java/de/vwgis/kafkatitanixexercise/KafkaTitanixExerciseApplication.java
+++ src/main/java/de/vwgis/kafkatitanixexercise/KafkaTitanixExerciseApplication.java
@@ -1,21 +1,9 @@
 package de.vwgis.kafkatitanixexercise;
 
-import org.apache.kafka.streams.KeyValue;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Materialized;
-import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.cloud.stream.binder.kafka.streams.annotations.KafkaStreamsProcessor;
-import org.springframework.messaging.handler.annotation.SendTo;
-
-import java.util.Arrays;
-import java.util.Date;
 
 
-//@EnableBinding(KafkaStreamsProcessor.class)
 @SpringBootApplication
 public class KafkaTitanixExerciseApplication {
 
@@ -24,20 +12,5 @@ public class KafkaTitanixExerciseApplication {
     }
 
 
-//    @StreamListener("input")
-//    @SendTo("output")
-//    public KStream<?, WordCount> process(KStream<Object, String> input) {
-//
-//        return input
-//                .flatMapValues(
-//                        value -> Arrays.asList(value.toLowerCase().split("\\W+")))
-//                .map((key, value) -> new KeyValue<>(value, value))
-//                .groupByKey()
-//                .windowedBy(TimeWindows.of(5000)
-//                        .count(Materialized.as("wordcounts"))
-//                        .toStream()
-//                        .map((key, value) ->
-//                                new KeyValue<>(null, new WordCount(key.key(), value));
-//    }
 
 }
