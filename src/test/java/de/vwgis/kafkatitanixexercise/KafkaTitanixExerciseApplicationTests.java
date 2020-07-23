43dabf8484a7f2514da82f5a61f8dff6f11ec420
UA2LFHQ <vilius.kukanauskas@vwgis.de>
2020-07-23 15:01:36 +0200

    addd postman project

--- src/test/java/de/vwgis/kafkatitanixexercise/KafkaTitanixExerciseApplicationTests.java
+++ src/test/java/de/vwgis/kafkatitanixexercise/KafkaTitanixExerciseApplicationTests.java
@@ -2,8 +2,6 @@ package de.vwgis.kafkatitanixexercise;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.binder.kafka.streams.annotations.KafkaStreamsProcessor;
 
 @SpringBootTest
 class KafkaTitanixExerciseApplicationTests {
